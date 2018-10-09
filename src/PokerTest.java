fdc4515011492e46cdf8c3dceb637c1ab4259247
matrix945 <frankzhang945@gmail.com>
2018-10-09 19:30:47 -0400

    Add a hello world test
    add a hello world test

---
+++ src/PokerTest.java
@@ -0,0 +1,21 @@
+import org.junit.After;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class PokerTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+
+//    @After
+//    public void after() {
+//        System.out.println("after");
+//    }
+
+
+}
